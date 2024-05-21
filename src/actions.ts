ada4e82ce11f5a54abaf91186f77da59360c69a5
ramatc <rtanquiascornejo@gmail.com>
2024-05-21 14:59:38 -0300

    Integrando API de Replicate

--- src/actions.ts
+++ src/actions.ts
@@ -1,14 +1,37 @@
 "use server";
 
-import { Prediction } from "@/types";
+import Replicate from "replicate";
 
-import { unstable_noStore as noStore } from "next/cache";
+const replicate = new Replicate({
+  auth: process.env.REPLICATE_API_TOKEN,
+});
 
-export async function createRemovePrediction(
-  formData: FormData
-): Promise<Prediction> {
-  noStore();
+export async function removeBackground(formData: FormData): Promise<any> {
+  const imageFile = formData.get("image") as File;
 
+  const imageUrl = await fetch(
+    `https://api.cloudinary.com/v1_1/ramatc/image/upload?upload_preset=replicate&folder=replicate`,
+    {
+      method: "PUT",
+      body: imageFile,
+    }
+  )
+    .then((res) => res.json() as Promise<{ secure_url: string }>)
+    .then(({ secure_url }) => secure_url);
+
+  const output = await replicate.run(
+    "cjwbw/rembg:fb8af171cfa1616ddcf1242c093f9c46bcada5ad4cf6f2fbe8b81b330ec5c003",
+    {
+      input: {
+        image: imageUrl,
+      },
+    }
+  );
+
+  return output;
+}
+
+export async function restoreColor(formData: FormData): Promise<any> {
   const imageUrl = await fetch(
     `https://api.cloudinary.com/v1_1/ramatc/image/upload?upload_preset=replicate&folder=replicate`,
     {
@@ -19,153 +42,15 @@ export async function createRemovePrediction(
     .then((res) => res.json() as Promise<{ secure_url: string }>)
     .then(({ secure_url }) => secure_url);
 
-  const prediction = await fetch(
-    "https://replicate.com/api/models/cjwbw/rembg/versions/fb8af171cfa1616ddcf1242c093f9c46bcada5ad4cf6f2fbe8b81b330ec5c003/predictions",
+  const output = await replicate.run(
+    "piddnad/ddcolor:ca494ba129e44e45f661d6ece83c4c98a9a7c774309beca01429b58fce8aa695",
     {
-      headers: {
-        accept: "application/json",
-        "accept-language": "es-ES,es;q=0.9",
-        "cache-control": "no-cache",
-        "content-type": "application/json",
-        pragma: "no-cache",
-        priority: "u=1, i",
-        "sec-ch-ua":
-          '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
-        "sec-ch-ua-mobile": "?1",
-        "sec-ch-ua-platform": '"Android"',
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-origin",
-        "x-csrftoken": "A5sL51Out2VApJoQFJhlzUlGHa3UrENR",
+      input: {
+        image: imageUrl,
+        model_size: "large",
       },
-      referrer: "https://replicate.com/cjwbw/rembg",
-      referrerPolicy: "same-origin",
-      body: JSON.stringify({
-        input: {
-          image: imageUrl,
-        },
-        stream: false,
-      }),
-      method: "POST",
-      mode: "cors",
-      credentials: "include",
     }
-  ).then((res) => res.json() as Promise<Prediction>);
+  );
 
-  return prediction;
-}
-
-export async function getRemovePrediction(id: string) {
-  noStore();
-
-  return fetch("https://replicate.com/api/predictions/" + id, {
-    headers: {
-      accept: "*/*",
-      "accept-language": "es-ES,es;q=0.9",
-      baggage:
-        "sentry-public_key=3dc017e574684610bbc7fd3b5519a4e8,sentry-trace_id=0bdb5ebe88fe428ca6c35dd71bb846cf,sentry-sample_rate=0.1",
-      "cache-control": "no-cache",
-      pragma: "no-cache",
-      priority: "u=1, i",
-      "sec-ch-ua":
-        '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
-      "sec-ch-ua-mobile": "?1",
-      "sec-ch-ua-platform": '"Android"',
-      "sec-fetch-dest": "empty",
-      "sec-fetch-mode": "cors",
-      "sec-fetch-site": "same-origin",
-      "sentry-trace": "0bdb5ebe88fe428ca6c35dd71bb846cf-8176464f7e4193e6-0",
-    },
-    referrer:
-      "https://replicate.com/cjwbw/rembg?prediction=avetbx74r5rgm0cf7ax9azw27w",
-    referrerPolicy: "same-origin",
-    body: null,
-    method: "GET",
-    mode: "cors",
-    credentials: "include",
-  }).then((res) => res.json() as Promise<Prediction>);
-}
-
-export async function createRestorePrediction(
-  formData: FormData
-): Promise<Prediction> {
-  noStore();
-
-  const imageUrl = await fetch(
-    `https://api.cloudinary.com/v1_1/ramatc/image/upload?upload_preset=replicate&folder=replicate`,
-    {
-      method: "PUT",
-      body: formData.get("image") as File,
-    }
-  )
-    .then((res) => res.json() as Promise<{ secure_url: string }>)
-    .then(({ secure_url }) => secure_url);
-
-  const prediction = await fetch(
-    "https://replicate.com/api/models/piddnad/ddcolor/versions/ca494ba129e44e45f661d6ece83c4c98a9a7c774309beca01429b58fce8aa695/predictions",
-    {
-      headers: {
-        accept: "application/json",
-        "accept-language": "es-ES,es;q=0.9",
-        "cache-control": "no-cache",
-        "content-type": "application/json",
-        pragma: "no-cache",
-        priority: "u=1, i",
-        "sec-ch-ua":
-          '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
-        "sec-ch-ua-mobile": "?1",
-        "sec-ch-ua-platform": '"Android"',
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-origin",
-        "x-csrftoken": "A5sL51Out2VApJoQFJhlzUlGHa3UrENR",
-      },
-      referrer:
-        "https://replicate.com/piddnad/ddcolor?prediction=54t51d0m1drgm0cfa44r2nq81m&output=json",
-      referrerPolicy: "same-origin",
-      body: JSON.stringify({
-        input: {
-          image: imageUrl,
-          model_size: "large",
-        },
-        stream: false,
-      }),
-      method: "POST",
-      mode: "cors",
-      credentials: "include",
-    }
-  ).then((res) => res.json() as Promise<Prediction>);
-
-  return prediction;
-}
-
-export async function getRestorePrediction(id: string) {
-  noStore();
-
-  return fetch("https://replicate.com/api/predictions/" + id, {
-    headers: {
-      accept: "*/*",
-      "accept-language": "es-ES,es;q=0.9",
-      baggage:
-        "sentry-public_key=3dc017e574684610bbc7fd3b5519a4e8,sentry-trace_id=93bf531fb67c4a10959156f143b1e5c8,sentry-sample_rate=0.1",
-      "cache-control": "no-cache",
-      pragma: "no-cache",
-      priority: "u=1, i",
-      "sec-ch-ua":
-        '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
-      "sec-ch-ua-mobile": "?1",
-      "sec-ch-ua-platform": '"Android"',
-      "sec-fetch-dest": "empty",
-      "sec-fetch-mode": "cors",
-      "sec-fetch-site": "same-origin",
-      "sentry-trace": "93bf531fb67c4a10959156f143b1e5c8-ad2fcbc2272710b1-0",
-    },
-    referrer:
-      "https://replicate.com/piddnad/ddcolor?prediction=1rmy27hhe9rgp0cfa4x9wabjzr",
-    referrerPolicy: "same-origin",
-    body: null,
-    method: "GET",
-    mode: "cors",
-    credentials: "include",
-  }).then((res) => res.json() as Promise<Prediction>);
+  return output;
 }
---
+++ src/types.d.ts
@@ -0,0 +1,17 @@
+declare module "react" {
+  interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
+    orientation?: "horizontal" | "vertical";
+    legacyClipCompat?: boolean;
+  }
+}
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "two-up": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+    }
+  }
+}
